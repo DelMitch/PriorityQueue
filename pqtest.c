87b8298399c4a42725a43b6bb4229004d0280f4f
amitchell <amitchell@alaska.edu>
2019-04-07 16:26:23 -0800

    Achieve 'Hello world' for testing file

---
+++ pqtest.c
@@ -0,0 +1,27 @@
+/******************************
+ * Addeline Mitchell
+ * April 2019
+ ******************************
+ * Source file for testing the
+ * priority queue
+ *****************************/
+
+// Include std libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Include Check for unit testing
+#include <check.h>
+
+// Include PQ header file
+#include "priorityqueue.h"
+ 
+
+int main ()
+{
+   // write test cases
+   printf("Hello world!\n");
+  
+   return 0;
+}
